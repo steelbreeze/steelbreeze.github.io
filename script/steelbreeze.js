ed3bb0f07b58dcd3f0ccffd36fa0c41e36197ef1
mesmo <mesmo@steelbreeze.net>
2017-11-02 20:00:45 +0000

    Change menu links

--- script/steelbreeze.js
+++ script/steelbreeze.js
@@ -4,9 +4,9 @@ var menuMain = document.getElementById("menuMain");
 
 // add menu items
 menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/">Home</a>');
-menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/ea/services/">Enterprise architecture</a>');
-menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/da/services/">Data architecture</a>');
-menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/dt/services/">Digital transformation</a>');
+menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/ea/">Enterprise architecture</a>');
+menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/da/">Data architecture</a>');
+menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/dt/">Digital transformation</a>');
 menuMain.insertAdjacentHTML('beforeend', '<p class="menuItem disabled">Technology leadership</p>');
 menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/os/">Open source</a>');
 
