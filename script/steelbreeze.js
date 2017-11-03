b846085315e10c1d358e9a202cb93bce90420d02
mesmo <mesmo@steelbreeze.net>
2017-11-03 10:12:21 +0000

    Better menus....

--- script/steelbreeze.js
+++ script/steelbreeze.js
@@ -4,19 +4,17 @@ var menuMain = document.getElementById("menuMain");
 
 // add menu items
 menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/">Home</a>');
-menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/ea/">Enterprise architecture</a>');
-menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/da/">Data architecture</a>');
-menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/dt/">Digital transformation</a>');
-menuMain.insertAdjacentHTML('beforeend', '<p class="menuItem disabled">Technology leadership</p>');
-menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/os/">Open source</a>');
+menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/ea/">Enterprise&nbsp;architecture</a>');
+menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/da/">Data&nbsp;architecture</a>');
+menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/dt/">Digital&nbsp;transformation</a>');
+menuMain.insertAdjacentHTML('beforeend', '<p class="menuItem disabled">Technology&nbsp;leadership</p>');
+menuMain.insertAdjacentHTML('beforeend', '<a class="menuItem" href="/os/">Open&nbsp;source</a>');
 
 // show/hide the main menu
 menuToggle.onclick = function () {
 	if (menuMain.style.display !== "block") {
-		menuToggle.className = "fa fa-chevron-up"
 		menuMain.style.display = "block";
 	} else {
-		menuToggle.className = "fa fa-chevron-down"
 		menuMain.style.display = "none";
 	}
 };
