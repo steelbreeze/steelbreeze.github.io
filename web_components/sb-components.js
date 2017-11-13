c0dc4fe841785b35ad22a19d30badd3b3e27c0c1
mesmo <mesmo@steelbreeze.net>
2017-11-13 09:00:51 +0000

    Hide the web component code in an anonymous function and call it

--- web_components/sb-components.js
+++ web_components/sb-components.js
@@ -1,9 +1,11 @@
-class SBFooter extends HTMLElement {
-	connectedCallback() {
-		var date = new Date();
+(function () {
+	class SBFooter extends HTMLElement {
+		connectedCallback() {
+			var date = new Date();
 
-		this.innerHTML = this.innerHTML + '<p>Copyright &copy; ' + date.getFullYear() + ' Steelbreeze Limited</p>';
+			this.innerHTML = this.innerHTML + '<p>Copyright &copy; ' + date.getFullYear() + ' Steelbreeze Limited</p>';
+		}
 	}
-}
 
-window.customElements.define('sb-footer', SBFooter);
+	window.customElements.define('sb-footer', SBFooter);
+})();
