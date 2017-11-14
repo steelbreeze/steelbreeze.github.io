2c1230415883cf83c2cf3055ac6a20ef7f760f50
mesmo <mesmo@steelbreeze.net>
2017-11-14 09:23:22 +0000

    Cleaner implementation of sb-menu

--- web_components/sb-components.js
+++ web_components/sb-components.js
@@ -1,35 +1,45 @@
 (function () {
-    const menuItems = [
-        { label: 'Home', link: '/' },
-        { label: 'Enterprise architecture', link: '/ea/services/' },
-        { label: 'Data architecture', link: '/da/services/' },
+	const menuItems = [
+		{ label: 'Home', link: '/' },
+		{ label: 'Enterprise architecture', link: '/ea/services/' },
+		{ label: 'Data architecture', link: '/da/services/' },
 		{ label: 'Digital transformation', link: '/dt/services/' },
-        { label: 'Technology leadership', link: '/leadership/services/' },
-        { label: 'Open source', link: '/os/' }
+		{ label: 'Technology leadership', link: '/leadership/services/' },
+		{ label: 'Open source', link: '/os/' }
 	];
-	
-    class SBLogo extends HTMLElement {
-        connectedCallback() {
-            this.innerHTML = '<span class="start">steel</span><span class="end">breeze</span>';
-        }
+
+	class SBLogo extends HTMLElement {
+		connectedCallback() {
+			this.innerHTML = '<span class="start">steel</span><span class="end">breeze</span>';
+		}
 	};
-	
-    class SBMenu extends HTMLElement {
-        connectedCallback() {
-			const current = this.getAttribute('data-current');
-			
-            this.innerHTML = menuItems.map(item => item.label === current ? `<p>${item.label}</p>` : `<a href="${item.link}">${item.label}</a>`).join('');
-        }
+
+	class SBMenu extends HTMLElement {
+		get current() {
+			return this.getAttribute('current');
+		}
+
+		connectedCallback() {
+			this.innerHTML = menuItems.map(menuItem => (menuItem.label === this.current ? makeP : makeA)(menuItem)).join('');
+		}
+
+		makeP(menuItem) {
+			return `<a href="${menuItem.link}">${menuItem.label}</a>`;
+		}
+
+		makeA(menuItem) {
+			return `<p>${menuItem.label}</p>`;
+		}
 	};
-	
-    class SBFooter extends HTMLElement {
-        connectedCallback() {
-            const date = new Date();
-            this.innerHTML = this.innerHTML + '<p>Copyright &copy; ' + date.getFullYear() + ' Steelbreeze Limited</p>';
-        }
+
+	class SBFooter extends HTMLElement {
+		connectedCallback() {
+			const date = new Date();
+			this.innerHTML = this.innerHTML + '<p>Copyright &copy; ' + date.getFullYear() + ' Steelbreeze Limited</p>';
+		}
 	};
-	
-    window.customElements.define('sb-logo', SBLogo);
-    window.customElements.define('sb-menu', SBMenu);
-    window.customElements.define('sb-footer', SBFooter);
+
+	window.customElements.define('sb-logo', SBLogo);
+	window.customElements.define('sb-menu', SBMenu);
+	window.customElements.define('sb-footer', SBFooter);
 })();
